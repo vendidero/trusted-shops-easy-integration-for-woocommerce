a61badf64e31bd5200b66909d4f46d664f0194e4
vendidero <info@vendidero.de>
2022-05-27 15:32:40 +0200

    Adding /dist directory to release

---
+++ dist/interfaces/settings.d.ts
@@ -0,0 +1,15 @@
+import type { Channel } from './channel';
+import type { Trustbadge } from './trustbadge';
+import type { Widgets } from './widgets';
+export interface Settings {
+    client_id: string;
+    client_secret: string;
+    channels: Channel[];
+    enable_invites: string[];
+    trustbadges: {
+        [key: string]: Trustbadge;
+    };
+    widgets: {
+        [key: string]: Widgets;
+    };
+}
---
+++ dist/interfaces/trustbadge.d.ts
@@ -0,0 +1,6 @@
+import type { TrustbadgeChild } from './trustbadge-child';
+export interface Trustbadge {
+    id: string;
+    salesChannelRef: string;
+    children: TrustbadgeChild[];
+}
