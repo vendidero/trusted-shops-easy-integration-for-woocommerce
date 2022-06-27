8bed35612e73be38fc687de552a3c4e8e713603a
vendidero <info@vendidero.de>
2022-06-27 11:10:08 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '606e1e363e89e0ae542f45b3c690dbe08a0ce00f',
+        'name' => 'vendidero/trusted-shops-easy-integration-for-woocommerce',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-autoloader' => array(
+            'pretty_version' => 'v2.11.6',
+            'version' => '2.11.6.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
+            'aliases' => array(),
+            'reference' => 'a22f41ff845b8bd4591a933b282270f57dd5bc1d',
+            'dev_requirement' => false,
+        ),
+        'vendidero/trusted-shops-easy-integration-for-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '606e1e363e89e0ae542f45b3c690dbe08a0ce00f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
