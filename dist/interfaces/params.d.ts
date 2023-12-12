a4766f6d62bfc4603c4cef2c2a2b9560ec7496de
vendidero <info@vendidero.de>
2023-12-12 14:14:01 +0100

    Adding /dist directory to release

---
+++ dist/interfaces/params.d.ts
@@ -0,0 +1,19 @@
+import type { WidgetLocation } from './widget-location';
+import { OrderStatus } from "./order-status";
+export interface Params {
+    ajax_url: string;
+    update_settings_nonce: string;
+    get_settings_nonce: string;
+    export_orders_nonce: string;
+    disconnect_nonce: string;
+    locale: string;
+    name_of_system: string;
+    order_statuses: {
+        [key: string]: OrderStatus[];
+    };
+    version_of_system: string;
+    supports_estimated_delivery_date: boolean;
+    version: string;
+    widget_locations: [];
+    sales_channels: WidgetLocation[];
+}
